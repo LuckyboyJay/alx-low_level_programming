dcbecfe60d13860d99f8b6fa44266db6a9468005
Apeh Joshua <apehjoshua1@gmail.com>
2022-07-05 03:58:53 -0700

    task6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -13,11 +11,10 @@ int main(void)
   long long int ll;
   float d;
   char c;
-
-  printf("Size ofa char: %lu byte(s)\n", sizeof(c));
+  printf("Size of a char: %lu byte(s)\n", sizeof(c));
   printf("Size of an int: %lu byte(s)\n", sizeof(i));
   printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-  printf("Size of a long long int: %lu byte(s)\n" sizeof(ll));
+  printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
   printf("Size of a float: %lu byte(s)\n", sizeof(d));
   return (0);
 }
